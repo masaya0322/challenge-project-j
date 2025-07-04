81f5fc0360aa9413a72ba2de4eb460028d8468f8
dakamiyu <da.kami.yu@gmail.com>
2025-07-04 14:38:53 +0900

    Update main.py
    コメントアウトを削除し、関数名を修正しました。

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,7 @@
 from fastapi import FastAPI
 
-# FastAPIアプリを作成
 app = FastAPI()
 
-# ルート（エンドポイント）を定義
 @app.get("/api/hello")
-def read_root():
+def test_hello():
     return {"message": "Hello World"}
