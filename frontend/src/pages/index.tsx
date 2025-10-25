4ba8ac32ebb45e354a7e9dafc6bb6da4edff2147
masaya0322 <llmasa0322ll@yahoo.co.jp>
2025-10-25 17:55:47 +0900

    add:shadcn-setup,add-button

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,5 +1,7 @@
 // frontend/app/page.tsx
 
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-50 p-6">
@@ -10,9 +12,7 @@ export default function Home() {
       <p className="text-xl text-gray-700 mb-8 max-w-lg text-center">
         Tailwind CSS が有効化されています。ここからあなたの素晴らしい開発を始めましょう。
       </p>
-      <button className="px-6 py-3 bg-indigo-500 text-white font-semibold rounded-lg shadow-md hover:bg-indigo-700 transition duration-300 transform hover:scale-105">
-        開発スタート！
-      </button>
+      <Button>Click me !</Button>
       <footer className="mt-12 text-sm text-gray-400">
         Next.js + pnpm + Tailwind
       </footer>
