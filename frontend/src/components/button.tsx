e368b9ddb42d054da7bd302f05134449aa8c0ae3
toxll00 <22aj030@ms.dendai.ac.jp>
2025-11-17 12:06:27 +0900

    chore(frontend): apply review feedback for GameButton

--- frontend/src/components/button.tsx
+++ frontend/src/components/button.tsx
@@ -1,5 +1,6 @@
 "use client";
-import { Button as ShadcnButton} from "./ui/button";
+
+import { Button as ShadcnButton } from "./ui/button";
 import { Press_Start_2P } from "next/font/google";
 
 const pixelFont = Press_Start_2P({
@@ -12,10 +13,7 @@ type Button = {
   onClick: () => void;
 };
 
-export function Button({
-  label,
-  onClick,
-}: Button) {
+export function Button({ label, onClick }: Button) {
   const handleClick = () => {
     onClick?.();
   };
@@ -40,12 +38,13 @@ export function Button({
           transition
           active:translate-y-[2px]
         `}
-        style={{ backgroundColor: "#1F8BFF"
-         }}
+        style={{ backgroundColor: "#1F8BFF" }}
       >
         {label.toUpperCase()}
       </ShadcnButton>
+
       <div className="absolute -bottom-2 left-0 right-0 h-4 bg-[#0a5aa0] rounded-b-xl" />
+
       <div className="absolute -bottom-1 left-4 right-4 h-1 bg-white rounded-full opacity-90 pointer-events-none" />
     </div>
   );
