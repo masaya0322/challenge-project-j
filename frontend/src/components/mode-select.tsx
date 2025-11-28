d75ce9488aefe2b1e628c33582f6e533ce96b5ea
toxll00 <22aj030@ms.dendai.ac.jp>
2025-11-28 12:39:14 +0900

    feat: add mode select UI (#53)

---
+++ frontend/src/components/mode-select.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { Button } from "@/components/button";
+
+export function ModeSelect() {
+  return (
+    <div className="flex gap-12 justify-center items-center py-12">
+      <Button label="CLEAN NOW" onClick={() => {}} />
+      <Button label="SET TIME" onClick={() => {}} />
+    </div>
+  );
+}
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,48 +1,11 @@
-import { TitleScreen } from "@/components/screens/TitleScreen";
-import { ModeSelectScreen } from "@/components/screens/ModeSelectScreen";
+import { Layout } from "../components//layout";
+import { Button } from "../components/ui/button";
 import { TimerSettingScreen } from "@/components/screens/TimerSettingScreen";
-import { TimerRunningScreen } from "@/components/screens/TimerRunningScreen";
-import { StageScreen } from "@/components/screens/StageScreen";
-import { ResultScreen } from "@/components/screens/ResultScreen";
-import { ScreenType } from "@/types/game";
 
-const GamePage = () => {
-  const currentScreen: ScreenType = "result" as ScreenType;
-  switch (currentScreen) {
-    case "title":
-      // タイトル画面
-      return <TitleScreen />;
-
-    case "mode-select":
-      // モード選択画面
-      return (
-        <ModeSelectScreen />
-      );
-    case "timer-setting":
-      // タイマー設定画面
-      return (
-        <TimerSettingScreen />
-      );
-
-    case "timer-running":
-      // タイマー進行中画面
-      return (
-        <TimerRunningScreen />
-      );
-
-    case "stage":
-      // ゲームステージ画面
-      return (
-        <StageScreen />
-      );
-
-    case "result":
-      // 結果画面
-      return <ResultScreen />;
-
-    default:
-      return null;
-  }
+export default function Home() {
+  return (
+    <Layout>
+      <TimerSettingScreen />
+    </Layout>
+  );
 }
-
-export default GamePage;
